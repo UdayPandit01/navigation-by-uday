3983c1e06b37a01091d81545d4c725acce845a5f
UdayPandit01 <uday.pandit@netsolutions.com>
2023-03-07 18:26:40 +0530

    done

---
+++ src/components/login/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import { TouchableOpacity } from 'react-native';
+
+export default function Login() {
+    return (
+        <View>
+            <Text>welcome to login page</Text>
+        </View>  
+    );
+};
---
+++ src/root/index.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import Navigation from './navigate'
+
+export default function index() {
+  return (
+    <Navigation />
+  )
+}
